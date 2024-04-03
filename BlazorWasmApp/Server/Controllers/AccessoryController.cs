0619f8f7858eef765a874bbe412bfb62a2dcc322
Paulino Auyon <paulinoauyon@gmail.com>
2024-04-03 16:36:58 -0700

    Add accessory controller

---
+++ BlazorWasmApp/Server/Controllers/AccessoryController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using BlazorWasmApp.Server.Domain.Repositories;
+using BlazorWasmApp.Shared.Domain.Entities;
+using BlazorWasmApp.Shared.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccessoryController : ControllerBase
+    {
+        protected ApiResponse _response;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AccessoryController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _response = new ApiResponse();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Accessory>>> GetAll()
+        {
+            var result = await _unitOfWork.AccessoryRepository.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Accessory>> GetAccessory(int id)
+        {
+            var result = await _unitOfWork.AccessoryRepository.Get(x => x.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("Accessory not found.");
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Accessory>> AddAccessory(Accessory accessory)
+        {
+            var result = await _unitOfWork.AccessoryRepository.Add(accessory);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Accessory>>> UpdateAccessory(Accessory accessory)
+        {
+            var result = await _unitOfWork.AccessoryRepository.Update(accessory);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteAccessory(int id)
+        {
+            var result = await _unitOfWork.AccessoryRepository.Delete(id);
+            return Ok(result);
+        }
+    }
+}
--- BlazorWasmApp/Shared/Domain/Repositories/IUnityOfWork.cs
+++ BlazorWasmApp/Shared/Domain/Repositories/IUnityOfWork.cs
@@ -1,8 +1,11 @@
-﻿namespace BlazorWasmApp.Server.Domain.Repositories
+﻿using BlazorWasmApp.Shared.Domain.Repositories;
+
+namespace BlazorWasmApp.Server.Domain.Repositories
 {
     public interface IUnitOfWork : IDisposable
     {
         ICameraRepository CameraRepository { get; }
+        IAccessoryRepository AccessoryRepository { get; }
         int Save();
     }
 }
